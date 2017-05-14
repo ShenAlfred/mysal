407ae8d8d39ec021820349775f41e36efb148def
Alfred <shenyuanchen@vchangyi.com>
2017-05-14 13:06:24 +0800

    vuex test

---
+++ src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>子组件:</h1>
+    <span>{{ number }}</span>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default {
+      components: {},
+      computed: {
+        number () {
+          return this.$store.state.count
+        }
+      }
+    }
+</script>
